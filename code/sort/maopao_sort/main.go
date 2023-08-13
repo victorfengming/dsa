425c1ff3cb96d72b8d99a4befef146a30efe14ac
victor <victorfm@163.com>
2023-08-13 14:59:25 +0800

    冒泡排序, Written by victor

---
+++ code/sort/maopao_sort/main.go
@@ -0,0 +1,30 @@
+package main
+
+// 冒泡排序
+// Written by victor
+
+func MaoPaoSort(data []int) []int {
+	// 遍历数组,每次把最大的移动到数组的最后面
+	for j := len(data) - 1; j > 0; j-- {
+		for i := 0; i < j; i++ {
+			if data[i] <= data[i+1] {
+				// 如果左边的小, 就不动
+			} else {
+				// 如果左边的大,就交换数值
+				data[i] = data[i] ^ data[i+1]
+				data[i+1] = data[i] ^ data[i+1]
+				data[i] = data[i] ^ data[i+1]
+			}
+		}
+	}
+	return data
+}
+
+func main() {
+	// 构造数据
+	data := []int{
+		7, 3, 9, 6,
+	}
+	data_sorted := MaoPaoSort(data)
+	print(data_sorted)
+}
