f0e1fefab7847f1f51f3aa50b048b5bcab80ccd1
victor <victorfm@163.com>
2023-08-22 15:16:52 +0800

    通过栈(数组)构造一个队列

---
+++ code/queue/queue_by_stack_test.go
@@ -0,0 +1,34 @@
+package test
+
+import "testing"
+
+func TestName(t *testing.T) {
+
+}
+
+// 使用两个栈来, 模拟一个队列
+type VicQueue struct {
+	inStack  []int
+	outStack []int
+}
+
+//
+
+func Constructor() VicQueue {
+
+}
+
+func (this *VicQueue) Push(x int) {
+
+}
+
+func (this *VicQueue) Pop() int {
+
+}
+
+func (this *VicQueue) Peek() int {
+
+}
+func (this *VicQueue) Empty() bool {
+
+}
