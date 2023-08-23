cb3304d595aba80ff00eebd92ad5eebfa0f72719
victor <victorfm@163.com>
2023-08-23 14:54:36 +0800

    调整了一下注释

--- code/leetcode/2829/main.go
+++ code/leetcode/2829/main.go
@@ -1,8 +1,6 @@
 package main
 
 func minimumSum(n int, k int) int {
-	//从1开始遍历
-	// 使用索引遍历字符串
 	j := 1
 	jslice := make([]int, 0)
 	for i := 0; i < n; i++ {
@@ -12,11 +10,8 @@ func minimumSum(n int, k int) int {
 			j += 1
 			continue
 		}
-
 	mark:
-		// 判断k是否和前面的能够组成和为k的
-		// 遍历jslice
-		// 使用循环遍历空切片
+		// 判断k是否和前面的能够组成和为k的, 遍历jslice
 		for _, value := range jslice {
 			if j+value == k {
 				j += 1
@@ -25,12 +20,10 @@ func minimumSum(n int, k int) int {
 		}
 		// 如果符合条件, 就存放到数组中
 		jslice = append(jslice, j)
-		// 累加
-		j += 1
+		j += 1 // 累加
 	}
 	// 初始化一个变量用于存储总和
 	sum := 0
-
 	// 遍历数组并将值相加
 	for _, value := range jslice {
 		sum += value
