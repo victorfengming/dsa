f468d7df34acc9a2b50bfa2cad5b637fd9325dd8
victor <victorfm@163.com>
2023-08-23 17:56:54 +0800

    2829. k-avoiding 数组的最小总和 大神版本

--- code/leetcode/2829/main.go
+++ code/leetcode/2829/main.go
@@ -1,32 +1,17 @@
 package main
 
-func minimumSum(n int, k int) int {
-	j := 1
-	// 初始化一个变量用于存储总和
-	sum := 0
-	jmap := make(map[int]interface{}, 0)
-	i := 0
-	for {
-		if i >= n {
-			break
-		}
-		// 先判断j是不是比k大了如果比k大了就不会相等,直接存就行了
-		if j >= k {
-			sum += j
-			j += 1
-			i += 1
-			continue
-		}
-		if _, ok := jmap[j]; ok {
+func minimumSum(n, k int) int {
 
-		} else {
-			sum += j
-			jmap[k-j] = nil
-			i += 1
-		}
-		j += 1
+	m := min(k/2, n)
+	return (m*(m+1) + (k*2+n-m-1)*(n-m)) / 2
+
+}
+
+func min(a, b int) int {
+	if b < a {
+		return b
 	}
-	return sum
+	return a
 }
 
 func main() {
